f441afcb09b3450242bf4737b2063226e90770e3
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2017-11-10 18:20:16 -0800

    [CI] Fix asynchronous error in the custom script.

--- .ci_preparation.sh
+++ .ci_preparation.sh
@@ -4,5 +4,7 @@
 # When using this locally you may need sudo.
 PATH_DEB_CODEMETER=/tmp/codemeter.deb
 PATH_DEB_ENSENSO=/tmp/ensenso.deb
-curl -o $PATH_DEB_CODEMETER "https://download.ensenso.com/s/ensensosdk/download?files=codemeter_6.40.2402.501_amd64.deb" | dpkg -i $PATH_DEB_CODEMETER
-curl -o $PATH_DEB_ENSENSO "https://download.ensenso.com/s/ensensosdk/download?files=ensenso-sdk-2.0.147-x64.deb" | dpkg -i $PATH_DEB_ENSENSO
+curl -o $PATH_DEB_CODEMETER "https://download.ensenso.com/s/ensensosdk/download?files=codemeter_6.40.2402.501_amd64.deb"  # Using pipe to pass the downloaded file could cause a problem as pipe is asynchronous https://stackoverflow.com/questions/32135523/how-to-make-pipe-run-sequentially. So run dpkg in separate lines.
+curl -o $PATH_DEB_ENSENSO "https://download.ensenso.com/s/ensensosdk/download?files=ensenso-sdk-2.0.147-x64.deb"
+dpkg -i $PATH_DEB_CODEMETER
+dpkg -i $PATH_DEB_ENSENSO
