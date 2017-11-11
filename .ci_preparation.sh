7127eace62c2e7d1a9a78c03cba5e129c1fd2253
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2017-11-10 20:37:15 -0800

    [CI] Use RUNLEVEL for ensenso driver to be installed.

--- .ci_preparation.sh
+++ .ci_preparation.sh
@@ -2,6 +2,7 @@
 
 # Downloading proprietary deb files from https://www.ensenso.com/support/sdk-download, and install them.
 # When using this locally you may need sudo.
+export RUNLEVEL=3
 PATH_DEB_CODEMETER=/tmp/codemeter.deb
 PATH_DEB_ENSENSO=/tmp/ensenso.deb
 curl -o $PATH_DEB_CODEMETER "https://download.ensenso.com/s/ensensosdk/download?files=codemeter_6.40.2402.501_amd64.deb"  # Using pipe to pass the downloaded file could cause a problem as pipe is asynchronous https://stackoverflow.com/questions/32135523/how-to-make-pipe-run-sequentially. So run dpkg in separate lines.
