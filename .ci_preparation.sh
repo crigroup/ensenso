d6802697e7410a50fd3fa8f6c11fda68970deb0b
fsuarez6 <fsuarez6@gmail.com>
2017-11-27 15:05:45 +0800

    List ensenso-sdk files for debugging

--- .ci_preparation.sh
+++ .ci_preparation.sh
@@ -4,3 +4,5 @@ echo "This is only required for the docker container"
 printf "#!/bin/sh\nexit 0" > /usr/sbin/policy-rc.d
 # Install the driver
 ./scripts/install_driver.sh
+echo "List ensenso-sdk files"
+dpkg -L ensenso-sdk
