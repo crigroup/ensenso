155963266209f9c896428dd244a3447538a074d8
fsuarez6 <fsuarez6@gmail.com>
2017-11-27 14:51:59 +0800

    Configure policy-rc.d for codemeter

---
+++ .ci_preparation.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Update content of policy-rc.d so that codemeter can be started"
+echo "This is only required for the docker container"
+printf "#!/bin/sh\nexit 0" > /usr/sbin/policy-rc.d
+# Install the driver
+./scripts/install_driver.sh
